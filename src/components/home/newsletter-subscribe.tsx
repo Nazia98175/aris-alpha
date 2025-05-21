5a19c3f162cc92cb6067d29903577ed936794264
Adnan Abbas <mirzaabbas171@gmail.com>
2025-05-21 22:17:31 +0500

    feat: enhance newsletter subscription component with updated messaging and improved layout for better user engagement

--- src/components/home/newsletter-subscribe.tsx
+++ src/components/home/newsletter-subscribe.tsx
@@ -50,12 +50,10 @@ const NewsLetterSubscribe = () => {
         <Container className="relative flex flex-col items-center gap-7 text-center sm:gap-10">
             <div className="relative">
                 <div className="relative z-10 space-y-5" id="subscribe">
-                    <h2 className="text-3xl font-medium sm:text-7xl">
-                        Subscribe <br />
-                        to our Newsletter
-                    </h2>
+                    <h2 className="text-3xl font-medium sm:text-7xl">Stay Ahead of the Market</h2>
                     <p className="text-muted-foreground max-w-5xl text-sm sm:text-base">
-                        Get weekly update about our product on your email, no spam guaranteed we promise ✌️
+                        Get timely trading insights, market sentiment, and strategic signals—delivered straight to your
+                        inbox.
                     </p>
                 </div>
 
@@ -81,7 +79,7 @@ const NewsLetterSubscribe = () => {
             ></div>
 
             <Form {...form}>
-                <div className="flex w-full max-w-3xl gap-4 overflow-hidden rounded-xl border border-white/10 bg-white/5 px-5 py-3 sm:px-10 sm:py-7">
+                <div className="flex w-full max-w-3xl flex-col gap-4 overflow-hidden rounded-xl border border-white/10 bg-white/5 px-5 py-3 sm:flex-row sm:px-10 sm:py-7">
                     <div className="flex flex-1 items-center gap-4">
                         <FormInput
                             control={form.control}
@@ -89,7 +87,7 @@ const NewsLetterSubscribe = () => {
                             className="w-full"
                             startIcon={<Mail />}
                             inputClassName="sm:h-14 h-10 placeholder:text-muted-foreground w-full text-sm focus:outline-0 sm:text-base"
-                            placeholder="email123@gmail.com"
+                            placeholder="Enter your email to receive real-time signal breakdowns and outlooks."
                         />
                     </div>
                     <LoadingButton
@@ -98,7 +96,7 @@ const NewsLetterSubscribe = () => {
                         loading={newsletterSubscribeMutation.isPending}
                         onClick={form.handleSubmit((values) => newsletterSubscribeMutation.mutate(values))}
                     >
-                        Subscribe Now
+                        Get Market Insights
                     </LoadingButton>
                 </div>
             </Form>

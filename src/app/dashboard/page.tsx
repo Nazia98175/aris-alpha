1f7e85ffc10d0d369922b5e5b3ff05be91e3c137
Tanu chaudhary <kaurtanu213@gmail.com>
2025-06-06 12:09:46 +0530

    remove error

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,17 +2,20 @@ import Chart from '@/components/dashboard/chart'
 import Header from '@/components/dashboard/header'
 import React from 'react'
 import { tradeData } from '../actions/trade.actions'
+import DashboardLayout from './layout'
 
 const page = async () => {
     const trade = await tradeData()
     return (
-        <div className="flex h-full w-full flex-col space-y-5 sm:space-y-10">
-            <Header />
+        <DashboardLayout>
+            <div className="flex h-full w-full flex-col space-y-5 sm:space-y-10">
+                <Header />
 
-            <div className="grow">
-                <Chart data={trade} />
+                <div className="grow">
+                    <Chart data={trade} />
+                </div>
             </div>
-        </div>
+        </DashboardLayout>
     )
 }
 

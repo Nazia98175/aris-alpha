5701aa37292a69ae3055b640242b728129e70e8d
Tanu chaudhary <kaurtanu213@gmail.com>
2025-06-06 09:29:56 +0530

    change loader ui

--- src/components/onboard/LoaderSpinner.tsx
+++ src/components/onboard/LoaderSpinner.tsx
@@ -2,7 +2,7 @@
 const LoaderSpinner = () => {
     return (
         <div className="flex h-[300px] w-full items-center justify-center">
-            <div className="h-8 w-8 animate-spin rounded-full border-4 border-white border-t-[#2A64F6] sm:h-12 sm:w-12" />
+            <div className="h-8 w-8 animate-spin rounded-full border-4 border-white border-t-transparent sm:h-12 sm:w-12" />
         </div>
     )
 }

f092e4f9dbe2705ac9aa4c9e663ea96f7516f382
Gourav472 <149783900+Gourav472@users.noreply.github.com>
2025-07-04 10:06:21 +0530

    compound circle image changed with clear version

--- src/components/why-us/ProblemWeSolve.tsx
+++ src/components/why-us/ProblemWeSolve.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 const ProblemWeSolve = () => {
     return (
         <section className="w-full px-4 py-10 text-white lg:py-20 xl:px-0">
-            <div className="mx-auto flex w-full max-w-[1140px] flex-col items-center justify-between lg:flex-row">
+            <div className="mx-auto flex w-full max-w-[1140px] flex-col items-center gap-6 justify-between lg:flex-row">
                 <div className="font-poppins w-full max-w-full lg:max-w-[542px]">
                     <h2 className="secondary-heading mb-6">The Problem We Solve</h2>
                     <p className="description !text-waterwhite/[80%] mb-6 !leading-[160%]">
@@ -17,14 +17,13 @@ const ProblemWeSolve = () => {
                         acting with discipline.
                     </p>
                 </div>
-                <div className="flex w-full items-center justify-center lg:justify-end">
                     <Image
-                        src="/assets/homepage/webp/compound-circle.webp"
+                    className='max-w-[280px] sm:max-w-[320px] md:max-w-[401px] max-h-[400px] w-full'
+                        src="/assets/homepage/webp/compound-circle-clear.webp"
                         alt="22%+ Target Annual Compound"
-                        width={501.84}
+                        width={401}
                         height={400}
                     />
-                </div>
             </div>
         </section>
     )
